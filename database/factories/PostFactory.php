e3120e058259eeadbde3b6d3b5d8247d2761c63b
AndrewGotham <andrewgotham@hotmail.com>
2025-01-16 13:32:25 +0300

    migration and factory for the posts table

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -16,8 +17,14 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $title = $this->faker->unique()->sentence();
         return [
-            //
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'excerpt' => $this->faker->text(),
+            'body' => $this->faker->paragraphs(5, true),
+            'published' => $this->faker->boolean(),
+            'min_to_read' => $this->faker->randomDigitNotNull(),
         ];
     }
 }
--- database/migrations/2025_01_16_095009_create_posts_table.php
+++ database/migrations/2025_01_16_095009_create_posts_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpt')->nullable()->default(null)->comment('Summery of the post');
+            $table->longText('body');
+            $table->boolean('published')->default(false);
+            $table->unsignedBigInteger('min_to_read')->default(0);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
