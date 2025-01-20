e72eccd7f7b40bc52def71e4609680e0ffe49605
AndrewGotham <andrewgotham@hotmail.com>
2025-01-20 11:40:30 +0300

    some of frontedn design works

---
+++ resources/views/frontend/default/layouts/footer.blade.php
@@ -0,0 +1,88 @@
+<footer class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-4/5 bp-10 m-auto border-b-2 border-gray-100 dark:border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-800 dark:text-gray-100">
+                Paĝoj
+            </h3>
+            <ul class="py-4 sm:text-s pt-4 text-gray-500 dark:text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Ĉefa
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/blog">
+                        Blogo
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/login">
+                        Ensaluti
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/register">
+                        Registriĝi
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-900 dark:text-gray-100">
+                Trovu nin
+            </h3>
+            <ul class="py-4 sm:text-s pt-4 text-gray-500 dark:text-gray-400">
+                <li class="pb-1">
+                    <a href="#">
+                        Kion ni faras
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="#">
+                        Adreso
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="#">
+                        Telefono
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="#">
+                        Kontaktoj
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-900 dark:text-gray-100">
+                Lastaj afiŝoj
+            </h3>
+            <ul class="py-4 sm:text-s pt-4 text-gray-500 dark:text-gray-400">
+                <li class="pb-1">
+                    <a href="#">
+                        Ni ŝatas programi
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="#">
+                        Ni ŝatas dezajni
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="#">
+                        Kial do Laravel
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="#">
+                        Php estas la plej bona
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-800 dark:text-gray-100 pt-5 text-center">
+        Copyright 2025. <a href="https://t.me/sirandrewgotham">Sir Andrew Gotham</a>. All Rights Reserved.
+    </p>
+</footer>
